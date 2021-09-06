5e5c8152a6eb26d35561c481967b740d834a18ca
João Pedrosa <jrcspp@hotmail.com>
2021-09-06 14:14:08 +0100

    all done minus the stylings

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+
+const droneSchema = mongoose.Schema(
+    {
+        name: String,
+        propellers: Number,
+        maxSpeed: Number,
+    }
+);
+
+const Drone = mongoose.model("Drone", droneSchema);
+
+module.exports = Drone;
